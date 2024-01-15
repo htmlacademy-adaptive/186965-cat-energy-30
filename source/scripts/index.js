df9fb1c6c4137cb2d5ee74abbb4d273683f6578a
Victoria <vitagood.92@list.ru>
2024-01-15 23:01:29 +0300

    сделала планшетную версию формы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,91 +1,96 @@
-(function () {
-  const TABLET_WIDTH = 768;
-  const DESKTOP_WIDTH = 1300;
-  const slider = document.querySelector('.slider');
-  const scale = slider.querySelector('.slider__scale');
-  const grip = scale.querySelector('.slider__grip');
-  const before = slider.querySelector('.slider__image--before');
-  const after = slider.querySelector('.slider__image--after');
-  let scaleWidth;
-  // halfGripWidth,
-  let gripWidth;
 
-  const getElemWidth = function (elem) {
-    return parseInt(getComputedStyle(elem).width, 10);
-  };
+// (function () {
+//   const TABLET_WIDTH = 768;
+//   const DESKTOP_WIDTH = 1300;
+//   const slider = document.querySelector('.slider');
+//   const scale = slider.querySelector('.slider__scale');
+//   const grip = scale.querySelector('.slider__grip');
+//   const before = slider.querySelector('.slider__image--before');
+//   const after = slider.querySelector('.slider__image--after');
+//   let scaleWidth;
+//   let halfGripWidth;
+//   let gripWidth;
 
-
-  const getCoords = function (elem) {
-    const box = elem.getBoundingClientRect();
-    return box.left + pageXOffset;
-  };
-
-  const gripDownHandler = function (evtDown) {
-    const gripCoords = getCoords(grip);
-    const scaleCoords = getCoords(scale);
-    grip.classList.add('is_active');
+//   const getElemWidth = function (elem) {
+//     return parseInt(getComputedStyle(elem).width, 10);
+//   };
 
 
-    const shiftX = evtDown.pageX - gripCoords;
+//   const getCoords = function (elem) {
+//     const box = elem.getBoundingClientRect();
+//     console.log(box, elem)
+//     return box.left + pageXOffset;
+//   };
 
-    document.onmousemove = function (evtMove) {
-      let newLeft = evtMove.pageX - shiftX - scaleCoords;
+//   const gripDownHandler = function (evtDown) {
+//     const gripCoords = getCoords(grip);
+//     const scaleCoords = getCoords(scale);
+//     grip.classList.add('is_active')
+//     // grip.style.transition = 'none';
 
-      if (newLeft < 0) {
-        newLeft = 0;
-      }
+//     // console.log(gripCoords, scaleCoords)
 
-      const rightEdge = scaleWidth - gripWidth;
-      if (newLeft > rightEdge) {
-        newLeft = rightEdge;
-      }
+//     const shiftX = evtDown.pageX - gripCoords;
 
-      const gripValue = newLeft / rightEdge * 120;
-      grip.style.marginLeft = `${newLeft}px`;
+//     document.onmousemove = function (evtMove) {
+//       let newLeft = evtMove.pageX - shiftX - scaleCoords;
 
-      before.style.width = `${gripValue}%`;
-      after.style.width = `(100 - ${gripValue})%`;
+//       if (newLeft < 0) {
+//         newLeft = 0;
+//       }
+
+//         let rightEdge = scaleWidth - gripWidth;
+//         if (newLeft > rightEdge) {
+//           newLeft = rightEdge;
+//         }
+
+//         let gripValue = newLeft / rightEdge * 100;
+//         grip.style.marginLeft = newLeft + 'px';
+
+//         before.style.width = (gripValue) + '%' ;
+//         after.style.width = (100 - gripValue) + '%';
+
+//         console.log(gripValue)
+
+//       };
+
+//       document.onmouseup = function () {
+//         document.onmousemove = document.onmouseup = null;
+//         grip.classList.remove('is_active');
+
+//       };
+
+//       return false;
+//     };
+
+//     let addGripHandlers = function () {
+//       grip.addEventListener('mousedown', gripDownHandler);
+//     };
+
+//     let removeGripHandlers = function () {
+//       grip.removeEventListener('mousedown', gripDownHandler);
+//     };
 
 
-    };
+//   let initialize = function() {
+//     let viewport = document.documentElement.clientWidth || window.innerWidth;
 
-    document.onmouseup = function () {
-      document.onmousemove = document.onmouseup = null;
-      grip.classList.remove('is_active');
+//     if (viewport >= TABLET_WIDTH) {
+//       addGripHandlers();
+//     } else {
+//       removeGripHandlers();
+//     }
 
-    };
+//     sliderWidth = getElemWidth(slider);
+//     scaleWidth = getElemWidth(scale);
+//     gripWidth = getElemWidth(grip);
+//     halfGripWidth = sliderWidth / gripWidth/2
 
-    return false;
-  };
+//     before.style.width = '';
+//     after.style.width = '';
+//     grip.style.marginLeft = '';
+//   };
 
-  const addGripHandlers = function () {
-    grip.addEventListener('mousedown', gripDownHandler);
-  };
-
-  const removeGripHandlers = function () {
-    grip.removeEventListener('mousedown', gripDownHandler);
-  };
-
-
-  const initialize = function() {
-    const viewport = document.documentElement.clientWidth || window.innerWidth;
-
-    if (viewport >= TABLET_WIDTH) {
-      addGripHandlers();
-    } else {
-      removeGripHandlers();
-    }
-
-    // sliderWidth = getElemWidth(slider);
-    scaleWidth = getElemWidth(scale);
-    gripWidth = getElemWidth(grip);
-    // halfGripWidth = sliderWidth / gripWidth / 2;
-
-    before.style.width = '';
-    after.style.width = '';
-    grip.style.marginLeft = '';
-  };
-
-  window.addEventListener('load', initialize);
-  window.addEventListener('resize', initialize);
-})();
+//   window.addEventListener('load', initialize);
+//   window.addEventListener('resize', initialize);
+// })();
