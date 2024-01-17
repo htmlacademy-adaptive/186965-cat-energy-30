d83e668040bdaea2553df52f6f59d02a787e6478
Victoria <vitagood.92@list.ru>
2024-01-17 23:27:33 +0300

    Всё сверстала + начла поправлять ошибки линтера

--- source/scripts/mobile.menu.js
+++ source/scripts/mobile.menu.js
@@ -1,9 +1,9 @@
-const menuButtonElement = document.querySelector('.main-navigation__toggle');
+const menuButtonElement = document.querySelector('.page-header__toggle');
 const navigationListElement = document.querySelector('.main-navigation');
 
 menuButtonElement.addEventListener('click', (evt) => {
   evt.preventDefault();
 
-  menuButtonElement.classList.toggle('main-navigation__toggle--is-opened');
+  menuButtonElement.classList.toggle('page-header--is-opened');
   navigationListElement.classList.toggle('main-navigation--is-opened');
 });
